05c33178b8794e297257184435d10bce67f5e46b
GioLange2002 <giovanni.langella@mail.polimi.it>
2024-04-19 15:43:56 +0200

    Added classes GameServer and Server_IO

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/ServerSide/GameServer.java
@@ -0,0 +1,169 @@
+package main.java.it.polimi.ingsw.ServerSide;
+
+import main.java.it.polimi.ingsw.ServerSide.MainClasses.Game;
+import main.java.it.polimi.ingsw.ServerSide.MainClasses.MultipleGameManager;
+import main.java.it.polimi.ingsw.ServerSide.Utility.ServerConstants;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class GameServer extends Thread{
+
+    private DatagramSocket socket;
+    private Server_IO.ServerRMI_impl UpdatedRMI;
+
+    private Game game;
+    private int IntegerInString;
+
+    public GameServer(int Port)
+    {
+
+        try{
+            this.socket = new DatagramSocket(Port);
+        }catch (SocketException e){e.printStackTrace();}
+
+        try {
+            Registry reg = LocateRegistry.createRegistry(Port+1);
+
+            UpdatedRMI = new Server_IO.ServerRMI_impl();
+            reg.bind("GetUpdates", UpdatedRMI);
+
+
+
+        } catch (RemoteException | AlreadyBoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+    }
+
+
+    public void run() {
+        while (true) {
+
+            //SocketCommands
+            byte[] data = new byte[2048];
+            DatagramPacket packet = new DatagramPacket(data, data.length);
+
+            try {
+                socket.receive(packet);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            String[] message = new String(packet.getData()).trim().split(",");
+            //System.out.println("Client ["+packet.getAddress() +" "+ packet.getPort()  + "] > " + Arrays.toString(message));
+
+            String ack = "ack";
+            String username = message[1];
+            String response;
+
+            Game game = MultipleGameManager.getGameInstance(username);
+            assert game!= null;
+
+            switch (message[0]) {
+
+                case "SendUpdate":
+                    String Update = Server_IO.SocketUpdate(username);
+                    if(ServerConstants.getDebug()){System.out.println("Update:\n"+Update);}
+                    sendData(Update.getBytes(), packet.getAddress(), packet.getPort());
+                    break;
+
+                case  "GameStartedStatus":
+                    if(game.isGameStarted())
+                    {sendData("true".getBytes(), packet.getAddress(), packet.getPort());}
+                    else{sendData("false".getBytes(), packet.getAddress(), packet.getPort());}
+                    break;
+
+                case  "SendCurrentTurn":
+                    if(game.getCurrentPlayerTurn() == game.getPlayerNumber(username))
+                    {sendData("true".getBytes(), packet.getAddress(), packet.getPort());}
+                    else{sendData("false".getBytes(), packet.getAddress(), packet.getPort());}
+                    break;
+
+
+                //modifiers
+                case "Flip":
+                    IntegerInString = Integer.parseInt(message[2]);
+                    Server_IO.Flip(IntegerInString, username);
+                    sendData(ack.getBytes(), packet.getAddress(), packet.getPort());
+                    break;
+
+                case "Draw":
+                    IntegerInString = Integer.parseInt(message[2]);
+                    Server_IO.DrawCard(IntegerInString, username);
+                    sendData(ack.getBytes(), packet.getAddress(), packet.getPort());
+                    break;
+
+                case "ChooseGoalCard":
+                    IntegerInString = Integer.parseInt(message[2]);
+                    Server_IO.ChooseGoalCard(IntegerInString, username);
+                    sendData(ack.getBytes(), packet.getAddress(), packet.getPort());
+                    break;
+
+                case "PlaceStartingCard":
+                    IntegerInString = Integer.parseInt(message[2]);
+                    System.out.println("Placed StartingCard");
+                    Server_IO.PlaceStartingCard(IntegerInString, username);
+                    sendData(ack.getBytes(), packet.getAddress(), packet.getPort());
+                    break;
+
+                case "playCardByIndex":
+                    System.out.println("Requested Play " + message[2] + " " + message[3] + " " + message[4]);
+                    String returnValue = "false";
+
+                    BigInteger flag = BigInteger.valueOf(0);
+                    if(Server_IO.PlayCardByIndex(Integer.parseInt(message[2]), Integer.parseInt(message[3]),
+                            Integer.parseInt(message[4]), username)){returnValue = "true";}
+
+                    sendData(returnValue.getBytes(), packet.getAddress(), packet.getPort());
+                    break;
+
+                case "getNewPort":
+                    System.out.println("NewSocketPort = " + game.getPort());
+                    String newPort = "" + game.getPort();
+                    sendData(newPort.getBytes(), packet.getAddress(), packet.getPort());
+                    break;
+
+
+                case "JoinPackage":
+
+                    if(MultipleGameManager.getGameInstance(username)!=null){ response = "Connection Failed: username already present"; }
+                    else{ MultipleGameManager.JoinGame(username); response = "Joining";}
+                    sendData(response.getBytes(), packet.getAddress(), packet.getPort());
+                    break;
+
+                case "AttemptingReconnection":
+
+                    if(game.getPlayerNumber(username)!=5){ response = "Joining";}
+                    else{ response = "Username Not Present";}
+
+                    sendData(response.getBytes(), packet.getAddress(), packet.getPort());
+            }
+
+
+        }
+    }
+
+    public void sendData(byte[] data, InetAddress ipAddress, int port)
+    {
+        DatagramPacket packet = new DatagramPacket(data, data.length, ipAddress, port);
+        try {
+            socket.send(packet);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
+}
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/ServerSide/Server_IO.java
@@ -0,0 +1,165 @@
+package main.java.it.polimi.ingsw.ServerSide;
+
+
+import  main.java.it.polimi.ingsw.ServerSide.Rmi.ServerRMI;
+import  main.java.it.polimi.ingsw.ServerSide.MainClasses.Game;
+import  main.java.it.polimi.ingsw.ServerSide.MainClasses.MultipleGameManager;
+import  main.java.it.polimi.ingsw.ServerSide.MainClasses.UpdatePackage;
+import  main.java.it.polimi.ingsw.ServerSide.Table.Player;
+import  main.java.it.polimi.ingsw.ServerSide.Table.Table;
+import  main.java.it.polimi.ingsw.ServerSide.UpdateClasses.TableManager;
+import  main.java.it.polimi.ingsw.ServerSide.Utility.ServerConstants;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class Server_IO {
+
+    //Modifiers
+    public static void Flip(int position, String username){
+        Objects.requireNonNull(MultipleGameManager.getGameInstance(username)).getPlayerByUsername(username) .FlipCard(position);}
+
+    public static boolean PlayCardByIndex(int rowIndex, int columnsIndex, int id, String username) {
+       return TableManager.playCardByIndex(rowIndex, columnsIndex, id, username);   }
+    public static void DrawCard(int position, String username){ Table table = Objects.requireNonNull(MultipleGameManager.getGameInstance(username)).getRelatedTable();
+     table.DrawCard(position, username);  }
+    public static void PlaceStartingCard(int selectedCard, String username){ Game game = Objects.requireNonNull(MultipleGameManager.getGameInstance(username));
+        TableManager.PlaceStartingCard(selectedCard, game.getPlayerNumber(username) ,username); }
+    public static void ChooseGoalCard(int position, String username){
+        Player player = Objects.requireNonNull(MultipleGameManager.getGameInstance(username)).getPlayerByUsername(username);
+        if(position==3){ player.setCard(5, 0); }
+        if(position==5){ player.setCard(3,  player.getPrivateCardsID()[5]); player.setCard(5, 0);}
+    }
+
+
+
+    static double timePerTurn = (100000000.0); static long previousTime = System.nanoTime();
+
+    public static String SocketUpdate(String username)
+    {
+        Game gameInstance = MultipleGameManager.getGameInstance(username);
+        if(gameInstance==null){ return "null\n"; }
+
+        Player currentPlayer = gameInstance.getPlayerByUsername(username);
+        if(currentPlayer==null){ return "null\n"; }
+
+
+        StringBuilder UpdatedGame = new StringBuilder();
+
+        UpdatedGame.append(gameInstance.getPlayerCount()).append(";");
+        UpdatedGame.append(Arrays.toString(gameInstance.getRelatedTable().getPublicSpacesID())).append(";");
+        UpdatedGame.append(Arrays.toString(currentPlayer.getPrivateCardsID())).append(";");
+
+        StringBuilder GameBoard= new StringBuilder();
+        int[][] GameBoard_ID = gameInstance.getRelatedTable().getOccupiedSpaces()[gameInstance.getPlayerNumber(username)];
+
+        for(int RowIndex = 0; RowIndex< ServerConstants.getNumOfRows(); RowIndex++){
+            GameBoard.append(Arrays.toString(GameBoard_ID[RowIndex])).append(":");  }
+        GameBoard.append(";");
+
+        UpdatedGame.append(GameBoard);
+
+
+        for(Player player : gameInstance.getPlayers()){ UpdatedGame.append(player.getScoreBoard()[0]).append(",");}
+
+
+        return UpdatedGame.toString();
+    }
+
+    private static int getNewPort(String username)
+    { return  Objects.requireNonNull(MultipleGameManager.getGameInstance(username)).getPort();  }
+
+
+
+
+    public static class ServerRMI_impl extends UnicastRemoteObject implements ServerRMI
+    {
+
+        private int RMI_PlayerCount; private String username; private int RMI_CurrentTurn;
+        private UpdatePackage RMI_UpdatePackage; private Game game;
+
+        protected ServerRMI_impl() throws RemoteException{super();}
+
+
+        public int RMI_getCurrentPlayerCount() throws RemoteException{return this.RMI_PlayerCount;}
+        public int RMI_getCurrentPlayerTurn() throws RemoteException{return this.RMI_CurrentTurn;}
+
+        public int[] RMI_getPublicCardsID() throws RemoteException
+        {return this.RMI_UpdatePackage.getPublicCards();}
+        public int[][] RMI_getPlayerBoard() throws RemoteException
+        {return this.RMI_UpdatePackage.getChosenPlayerBoard();}
+
+        public int[] RMI_getPlayerHand() throws RemoteException
+        {return this.RMI_UpdatePackage.getChosenPlayerHand();}
+
+        public int[] RMI_getPlayersScores() throws RemoteException
+        {
+            List<Player> players = game.getPlayers();
+            int[] Scores = new int[game.getPlayerCount()];
+
+            for(int index=0; index<game.getPlayerCount(); index++)
+            { Scores[index] = players.get(index).getScoreBoard()[0]; }
+
+            return Scores;
+        }
+
+
+        //modifiers
+        public void RMI_Flip(int position) throws RemoteException{
+            Server_IO.Flip(position, this.username);}
+        public void RMI_DrawCard(int position) throws RemoteException{
+            Server_IO.DrawCard(position, this.username); this.game.changePlayerTurn();}
+        public boolean RMI_PlayCardByIndex(int rowIndex, int columnsIndex, int id) throws RemoteException{
+            return Server_IO.PlayCardByIndex(rowIndex, columnsIndex, id, this.username);}
+        public void RMI_ChooseGoalCard(int position){ ChooseGoalCard(position, this.username);}
+        public void RMI_PlaceStartingCard(int selectedCard) throws RemoteException{
+            Server_IO.PlaceStartingCard(selectedCard, this.username); }
+
+
+
+
+        public String JoinGame(String username) throws RemoteException
+        {
+            if(MultipleGameManager.JoinGame(username)){ this.game = MultipleGameManager.getGameInstance(username);
+                return "Connection attempt was successful"; }
+            return "Connection failed";
+        }
+        public String Reconnect(String username) throws RemoteException
+        {
+            if(MultipleGameManager.Reconnect(username) != null){ this.game = MultipleGameManager.getGameInstance(username);
+                return "Reconnecting to previous game"; }
+            return "Reconnection attempt failed: Username not found";
+        }
+        public int RMI_getNewPort(String username) throws RemoteException{  return getNewPort(username)  ;   }
+
+        public void update(String username) throws RemoteException {
+
+            if(System.nanoTime() == previousTime+timePerTurn){  System.out.println("Turn timer Expired"); previousTime=System.nanoTime();
+                game.changePlayerTurn();}
+
+
+            System.out.println("Updating RMI at "+username);
+            this.username = username;
+            this.RMI_CurrentTurn = this.game.getCurrentPlayerTurn();
+            this.RMI_PlayerCount = this.game.getPlayerCount();
+            this.RMI_UpdatePackage = this.game.sendUpdatePackage(username);
+
+        }
+
+        public boolean GameStarted(String username){ this.game = MultipleGameManager.getGameInstance(username);
+            assert this.game != null;
+            return (this.game.isGameStarted());}
+
+        public boolean isTurn(String username) throws RemoteException{return this.game.getCurrentPlayerTurn() == this.game.getPlayerNumber(username);}
+
+    }
+
+
+    //OnlyServerSide
+    public static int[] sendGridDimensions(){ return new int[]{20, 10};}
+
+
+}
